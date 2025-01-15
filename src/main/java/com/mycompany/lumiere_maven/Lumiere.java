d5b864c23e765d7e736197189ce529a7ff72820a
Heng-Yi-Kang <yikangheng@gmail.com>
2025-01-15 13:30:31 +0800

    gui commit

--- src/main/java/com/mycompany/lumiere_maven/Lumiere.java
+++ src/main/java/com/mycompany/lumiere_maven/Lumiere.java
@@ -2,67 +2,122 @@
 package com.mycompany.lumiere_maven;
 
 import java.util.List;
-import java.util.Scanner;
+//import java.util.Scanner;
+//import javafx.animation.FadeTransition;
+//import javafx.animation.ScaleTransition;
+import javafx.application.Application;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+//import javafx.util.Duration;
 
-public class Lumiere
+public class Lumiere extends Application
 {
+    @Override
+    public void start(Stage stage) {
+        List<Task> tasks = loadData.getTasks();
+        loadData.initialise(stage, tasks);
+        stage.setTitle("Lumiere");
+        stage.show();
+    }
+
+    public static Scene mainMenu(Stage stage, List<Task> tasks) {
+        Label title = new Label("Lumiere");
+        Label username = new Label("Welcome, " + loadData.getUsername());
+        
+        VBox viewBox = new VBox();
+        viewBox.setStyle("-fx-alignment: center;");
+
+        Label viewText = new Label("View Tasks");
+        viewBox.getChildren().add(viewText);
+        viewBox.setOnMouseClicked(e -> {
+            stage.setScene(view.viewScene(stage, tasks));
+        });
+
+        VBox create = new VBox();
+        create.setStyle("-fx-alignment: center;");
+
+        Label createText = new Label("Create Tasks");
+        create.getChildren().add(createText);
+        create.setOnMouseClicked(e -> {
+            stage.setScene(addtask.createScene(stage, tasks));
+        });
+
+        Button exit = new Button("Save and Exit");
+        exit.setOnAction(event -> {
+            loadData.saveTasks(tasks);
+            stage.close();
+        });
+        
+        VBox root = new VBox(10);
+        root.getChildren().addAll(title, username, viewBox, create, exit);
+        root.setAlignment(Pos.CENTER);
+        stage.setTitle("Main Menu");
+        
+        return new Scene(root, 1200, 1000);
+    }
+    
     public static void main(String[] args) 
     {
-        Scanner input = new Scanner(System.in);
-        List<Task> tasks = loadData.getTasks();
-        boolean status = true;
-        while(status)
-        {
-            view.lines();
-            loadData.initialise(input);
-            email.checkDeadlines(tasks);
-            System.out.printf("Welcome %s!\n", loadData.getUsername());
-            System.out.println("Lumiere – your guide to a brighter, more organized life.");
-            System.out.println("""
-                             Action:
-                             1. View tasks.
-                             2. Create new task.
-                             3. Complete a task.
-                             4. Edit a task
-                             5. Search for task(s).
-                             6. Delete a task.
-                             7. Analyse tasks.
-                             8. Save and Exit.""");
-            System.out.print("> ");
-            int action = input.nextInt();
-            input.nextLine();
-            switch(action)
-            {
-                case 1:
-                    view.viewTasks(tasks, input);
-                    break;
-                case 2:
-                    addtask.createTask(tasks);
-                    break;
-                case 3:
-                    CompleteTask2.markTaskAsComplete(input, tasks);
-                    break;
-                case 4:
-                    EditTask.editTask(tasks, input);
-                    break;
-                case 5:
-                    searchEngine.run(tasks, input);
-                    break;
-                case 6:
-                    addtask.deleteTask(tasks, input);
-                    break;
-                case 7:
-                    analyse.showCompletionStatus(tasks);
-                    analyse.showCompletionRate(tasks);
-                    analyse.showCategorizedTask(tasks);
-                    break;
-                case 8:
-                    System.out.println("See you again :)");
-                    status = false;
-                    break;
-            }
-        }
-        loadData.saveTasks(tasks);
+//        Scanner input = new Scanner(System.in);
+//        List<Task> tasks = loadData.getTasks();
+//        boolean status = true;
+//        while(status)
+//        {
+//            view.lines();
+//            loadData.initialise(input);
+//            email.checkDeadlines(tasks);
+//            System.out.printf("Welcome %s!\n", loadData.getUsername());
+//            System.out.println("Lumiere – your guide to a brighter, more organized life.");
+//            System.out.println("""
+//                             Action:
+//                             1. View tasks.
+//                             2. Create new task.
+//                             3. Complete a task.
+//                             4. Edit a task
+//                             5. Search for task(s).
+//                             6. Delete a task.
+//                             7. Analyse tasks.
+//                             8. Save and Exit.""");
+//            System.out.print("> ");
+//            int action = input.nextInt();
+//            input.nextLine();
+//            switch(action)
+//            {
+//                case 1:
+//                    view.viewTasks(tasks, input);
+//                    break;
+//                case 2:
+//                    addtask.createTask(tasks);
+//                    break;
+//                case 3:
+//                    CompleteTask2.markTaskAsComplete(input, tasks);
+//                    break;
+//                case 4:
+//                    EditTask.editTask(tasks, input);
+//                    break;
+//                case 5:
+//                    searchEngine.run(tasks, input);
+//                    break;
+//                case 6:
+//                    addtask.deleteTask(tasks, input);
+//                    break;
+//                case 7:
+//                    analyse.showCompletionStatus(tasks);
+//                    analyse.showCompletionRate(tasks);
+//                    analyse.showCategorizedTask(tasks);
+//                    break;
+//                case 8:
+//                    System.out.println("See you again :)");
+//                    status = false;
+//                    break;
+//            }
+//        }
+//        loadData.saveTasks(tasks);
+        launch();
     }
     
 }
--- src/main/java/com/mycompany/lumiere_maven/view.java
+++ src/main/java/com/mycompany/lumiere_maven/view.java
@@ -4,10 +4,125 @@
  */
 package com.mycompany.lumiere_maven;
 
+import static com.mycompany.lumiere_maven.Lumiere.mainMenu;
+import javafx.scene.paint.Color;
 import java.util.List;
 import java.util.Scanner;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableCell;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
 
 public class view {
+    
+    public static Scene viewScene(Stage stage, List<Task> tasks) {
+        Label l = new Label("View Tasks");
+
+        TableView<Task> tableView = new TableView<>();
+
+        TableColumn<Task, String> titleColumn = new TableColumn<>("Title");
+        titleColumn.setCellValueFactory(new PropertyValueFactory<>("title"));
+
+        TableColumn<Task, String> descriptionColumn = new TableColumn<>("Description");
+        descriptionColumn.setCellValueFactory(new PropertyValueFactory<>("description"));
+
+        TableColumn<Task, String> dateColumn = new TableColumn<>("Due Date");
+        dateColumn.setCellValueFactory(new PropertyValueFactory<>("DateStr"));
+
+        TableColumn<Task, Boolean> statusColumn = new TableColumn<>("Status");
+        statusColumn.setCellValueFactory(new PropertyValueFactory<>("status"));
+
+        TableColumn<Task, String> categoryColumn = new TableColumn<>("Category");
+        categoryColumn.setCellValueFactory(new PropertyValueFactory<>("category"));
+
+        TableColumn<Task, String> priorityColumn = new TableColumn<>("Priority");
+        priorityColumn.setCellValueFactory(new PropertyValueFactory<>("Priority"));
+
+        TableColumn<Task, String> dependsColumn = new TableColumn<>("Depends On");
+        dependsColumn.setCellValueFactory(new PropertyValueFactory<>("DependsStr"));
+
+        TableColumn<Task, String> intervalColumn = new TableColumn<>("Repeat");
+        intervalColumn.setCellValueFactory(new PropertyValueFactory<>("RecurrenceInterval"));
+
+        statusColumn.setCellFactory(column -> {
+            return new TableCell<Task, Boolean>() {
+                @Override
+                protected void updateItem(Boolean item, boolean empty) {
+                    super.updateItem(item, empty);
+
+                    if (empty || item == null) {
+                        setText(null);
+                        setStyle(""); 
+                    } else {
+                        String statusText = item ? "Completed" : "Incomplete";
+                        setText(statusText);
+
+                        if (item) {
+                            setTextFill(Color.GREEN); 
+                        } else {
+                            setTextFill(Color.RED); 
+                        }
+                    }
+                }
+            };
+        });
+
+        priorityColumn.setComparator((priority1, priority2) -> {
+            int priority1Value = sort.getPriorityValue(priority1);
+            int priority2Value = sort.getPriorityValue(priority2);
+            return Integer.compare(priority1Value, priority2Value);
+        });
+
+        tableView.getColumns().add(titleColumn);
+        tableView.getColumns().add(descriptionColumn);
+        tableView.getColumns().add(dateColumn);
+        tableView.getColumns().add(statusColumn);
+        tableView.getColumns().add(categoryColumn);
+        tableView.getColumns().add(priorityColumn);
+        tableView.getColumns().add(dependsColumn);
+        tableView.getColumns().add(intervalColumn);
+
+        ObservableList<Task> taskTable = FXCollections.observableArrayList(tasks);
+
+        tableView.setRowFactory(tv -> {
+            javafx.scene.control.TableRow<Task> row = new javafx.scene.control.TableRow<>();
+            row.setOnMouseClicked(event -> {
+                if (event.getClickCount() == 2 && !row.isEmpty()) {
+                    Task selectedTask = row.getItem();
+                    stage.setScene(EditTask.showTaskDetails(stage, tasks, selectedTask));
+                }
+            });
+            return row;
+        });
+
+        tableView.setItems(taskTable);
+        tableView.setColumnResizePolicy(TableView.CONSTRAINED_RESIZE_POLICY);
+        tableView.setFixedCellSize(50);
+        tableView.setMaxWidth(1500);
+
+        Button b = new Button("Main Menu");
+        b.setOnAction(e -> {
+            stage.setScene(mainMenu(stage, tasks));
+        });
+
+        VBox root = new VBox(10);
+        root.getChildren().addAll(l, tableView, b);
+        root.setAlignment(Pos.CENTER);
+        Scene scene = new Scene(root, 1200, 1000);
+        tableView.prefHeightProperty().bind(scene.heightProperty().divide(1.33));
+        tableView.prefWidthProperty().bind(scene.widthProperty().divide(1.33));
+        
+        stage.setTitle("View Tasks");
+        return scene;
+    }
     
     public static void viewTasks(List<Task> tasks, Scanner input)
     {
